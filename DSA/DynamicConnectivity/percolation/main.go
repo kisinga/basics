a2484b3c9be98a29f246a82780150238ae0f075e
Steve Kisinga <stevekisinga@microsoft.com>
2024-02-19 21:06:15 +0300

    EOD

--- DSA/DynamicConnectivity/percolation/main.go
+++ DSA/DynamicConnectivity/percolation/main.go
@@ -1,19 +1,50 @@
 package percolation
 
-import "github.com/kisinga/basics/DSA/DynamicConnectivity/percolation/models"
+import (
+	"math/rand"
 
-func New(n int) models.Data {
-	// n := models.Node{
-	// 	Row: 0,
-	// 	Col: 0,
-	// }
-	return models.Data{
-		// Grid: make([][]models.Node, n),
+	"github.com/kisinga/basics/DSA/DynamicConnectivity/percolation/models"
+)
+
+func New(n int) *models.Data {
+
+	data := models.Data{}
+
+	// connect the first row
+	for i := range n {
+		data.Connect(models.Node{Row: 0, Col: i}, models.VirtualRootTop)
 	}
+
+	// connect the last row to the virtual bottom root
+	for i := range n {
+		data.Connect(models.Node{Row: n - 1, Col: i}, models.VirtualRootBottom)
+	}
+
+	// initialize the rest of the grid
+	for i := 1; i < n; i++ {
+		for j := 0; j <= n; j++ {
+			data.AddNode(i, j)
+		}
+	}
+
+	return &data
+
 }
 
 func main() {
-	var data models.Percolation = New(10)
+
+	gridSize := 10
+	var data models.Percolation = New(gridSize)
 	data.Open(1, 1)
+
+	count := 0
+	// open random sites until the system percolates
+	for !data.Percolates() {
+		// create a random row and column restricted to the size of the grid
+		data.Open(rand.Intn(gridSize), rand.Intn(gridSize))
+		count++
+	}
+
+	// print the number of open sites
 
 }
--- DSA/DynamicConnectivity/percolation/models/main.go
+++ DSA/DynamicConnectivity/percolation/models/main.go
@@ -2,12 +2,30 @@ package models
 
 type Data struct {
 	// the grid
-	grid [][]Node
+	grid [][]RootNode
 }
 
 type Node struct {
 	Row int
 	Col int
+}
+
+type RootNode struct {
+	*Node
+}
+
+var VirtualRootTop = RootNode{
+	Node: &Node{
+		Row: -1,
+		Col: -1,
+	},
+}
+
+var VirtualRootBottom = RootNode{
+	Node: &Node{
+		Row: -2,
+		Col: -2,
+	},
 }
 
 type Percolation interface {
@@ -27,7 +45,20 @@ type Percolation interface {
 	Percolates() bool
 }
 
-func (p Data) Open(row int, col int) {
+func (p *Data) AddNode(row int, col int) {
+	p.grid[row][col] = RootNode{
+		Node: &Node{
+			Row: row,
+			Col: col,
+		},
+	}
+}
+
+func (p *Data) Connect(node Node, rootNode RootNode) {
+	p.grid[node.Row][node.Col] = rootNode
+}
+
+func (p *Data) Open(row int, col int) {
 
 }
 
