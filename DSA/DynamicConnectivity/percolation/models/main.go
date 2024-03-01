25968d47c3f078c4fc2f722e50e57126b667609d
Steve Kisinga <stevekisinga@microsoft.com>
2024-03-01 19:16:12 +0300

    simplify model

--- DSA/DynamicConnectivity/percolation/models/main.go
+++ DSA/DynamicConnectivity/percolation/models/main.go
@@ -6,21 +6,10 @@ type Node struct {
 	IsOpen bool
 }
 
-type Data struct {
-	Grid
-	RealSize   int
-	VTopPos    int
-	VBottomPos int
-}
-
 type Grid [][]Node
 
-func EmptyData(size int) *Data {
-	model := &Data{
-		RealSize:   size,
-		VTopPos:    size,
-		VBottomPos: size + 1,
-	}
+func EmptyData(size int) Grid {
+
 	// we add 2 rows, one for vTop and the other for vBottom
 	grid := make([][]Node, size+2)
 
@@ -34,30 +23,25 @@ func EmptyData(size int) *Data {
 			}
 		}
 	}
-
-	grid[model.VTopPos] = make([]Node, 1)
-	grid[model.VBottomPos] = make([]Node, 1)
 	// create the vtop and vbottom and connect each to itself
-	grid[model.VTopPos][0] = Node{
-		Row:    model.VTopPos,
+	grid[0][0] = Node{
+		Row:    0,
 		Col:    0,
 		IsOpen: false,
 	}
-	grid[model.VBottomPos][0] = Node{
-		Row:    model.VBottomPos,
+	grid[size+1][0] = Node{
+		Row:    0,
 		Col:    0,
 		IsOpen: false,
 	}
 
 	// connect the first row to the vTop and the last row to the VBottom
 	for i := 0; i < size; i++ {
-		grid[0][i] = grid[model.VTopPos][0]
-		grid[size-1][i] = grid[model.VBottomPos][0]
+		grid[0][i] = grid[0][0]
+		grid[size-1][i] = grid[size+1][0]
 	}
 
-	model.Grid = grid
-
-	return model
+	return grid
 }
 
 type Percolation interface {
